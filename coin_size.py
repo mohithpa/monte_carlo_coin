bede4cf0a288fa627dd1180f0eae9eeae202387f
mohithpa <mohithpa2@gmail.com>
2022-10-21 12:33:32 +0530

    function circle_checker removed

--- coin_size.py
+++ coin_size.py
@@ -16,18 +16,34 @@ Ncoin = 0
 N = 0
 
 radius_of_circle = 1.75
-def circle_checker(number1, number2,radius):
-    global N
-    global Ncoin
-    if (number1**2) + (number2**2) <= radius**2:
-        Ncoin = Ncoin + 1
-        N = N + 1
-    else:
-        N=N+1
+#def circle_checker(number1, number2,radius):
+#    global N
+#    global Ncoin
+#    if (number1**2) + (number2**2) <= radius**2:
+#        Ncoin = Ncoin + 1
+#        N = N + 1
+#    else:
+#        N=N+1
+shots = [10,100,1000,10000,100000, 10**6, 10**7]
+simulated_radius = []
+for j in shots:
+    for i in range(j):
+        a,b = random_shot(square_side)
+        if (a**2) + (b**2) <= radius_of_circle**2:
+            Ncoin = Ncoin + 1
+            N = N + 1
+        else: 
+            N = N + 1
+        #circle_checker(a,b,radius_of_circle)
 
-for i in range(100):
-    a,b = random_shot(square_side)
-    circle_checker(a,b,radius_of_circle)
+    areaaa = area_of_circle(square_side**2,Ncoin, N)
+    simulated_element = (areaaa/np.pi)**0.5
+    simulated_radius.append(simulated_element)
 
-areaaa = area_of_circle(square_side**2,Ncoin, N)
-print(areaaa)
+print(simulated_radius)
+plt.plot(shots,simulated_radius)
+plt.xscale("log")
+plt.xlabel("No of shots")
+plt.ylabel("radius")
+plt.title("Number of shots vs radius")
+plt.savefig("coin_size.png")
